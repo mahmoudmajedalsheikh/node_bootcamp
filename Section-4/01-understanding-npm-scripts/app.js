c149651e7a1563e688f2efac4c80f4fa2447777f
Mahmoud Majed <mahmoudmajed404@gmail.com>
2024-12-26 15:17:02 -0800

    start-0

--- Section-4/01-understanding-npm-scripts/app.js
+++ Section-4/01-understanding-npm-scripts/app.js
@@ -1,9 +1,11 @@
-const http = require('http');
+// /Users/engmahmoudmajed/Documents/My projects/node_bootcamp/Section-4/01-understanding-npm-scripts
 
+const http = require('http');
 const routes = require('./routes');
 
 console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
+
 
 server.listen(3000);
