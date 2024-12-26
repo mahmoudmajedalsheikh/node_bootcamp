d785f93ddf74b41123c3d23ddd8900d27f0a1f9b
Mahmoud Majed <mahmoudmajed404@gmail.com>
2024-12-26 15:57:26 -0800

    start-0.1

--- Section-4/01-understanding-npm-scripts/app.js
+++ Section-4/01-understanding-npm-scripts/app.js
@@ -2,7 +2,12 @@
 
 const http = require('http');
 const routes = require('./routes');
-
+/*
+* npm i nodemon            //as main dep
+* npm i nodemon --save-dev //as dev dep
+* npm i nodemon -g         //in machine
+*
+* */
 console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
